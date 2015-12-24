569435cb0abfe411cdb6ec8b6ab4424cd74a675a
Danny Willems <contact@danny-willems.be>
2015-12-24 11:19:18 +0100

    ex09 ok and tested

--- ex09/ft_print_comb2.ml
+++ ex09/ft_print_comb2.ml
@@ -1,1 +1,27 @@
-let ft_print_comb2 () = ""
+let ft_print_comb2 () =
+    let print_int_loc i =
+        if i < 10 then
+        begin
+            print_char '0';
+            print_int i
+        end
+        else
+            print_int i
+    in
+    let rec ft_print_comb2_loc i j =
+        print_string ", ";
+        print_int_loc i;
+        print_char ' ';
+        print_int_loc j;
+        if i = 98 && j = 99 then ()
+        else if j = 99 then ft_print_comb2_loc (i + 1) (i + 2)
+        else ft_print_comb2_loc i (j + 1)
+    in
+    print_int_loc 0;
+    print_char ' ';
+    print_int_loc 1;
+    ft_print_comb2_loc 0 2;
+    print_char '\n'
+
+let () =
+    ft_print_comb2()
